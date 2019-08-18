b087477ee5dc468f2c90e13f9cae7b47436f6843
corka149 <corka149@mailbox.org>
2019-08-18 14:09:23 +0200

    Added storing jarvis address in config service

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -1,16 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigService {
 
-  constructor() { }
+  private static JARVIS_ADDRESS = 'jarvisAddress';
+
+  private jarvisAddress = 'http://127.0.0.1:4000';
+
+  constructor(
+    private storage: Storage
+  ) {
+    storage.ready().then(localForage => {
+      localForage.getItem(ConfigService.JARVIS_ADDRESS).then((address: string) => {
+        this.jarvisAddress = !!address ? address : this.jarvisAddress;
+      });
+    });
+  }
 
   /**
    * getHost
    */
   public getHost(): string {
-    return 'http://localhost:4000';
+    return this.jarvisAddress;
+  }
+
+  public setHost(jarvisAddress: string) {
+    localStorage.setItem(ConfigService.JARVIS_ADDRESS, jarvisAddress);
+    this.jarvisAddress = jarvisAddress;
   }
 }
